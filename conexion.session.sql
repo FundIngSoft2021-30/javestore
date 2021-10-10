061f162b3cef47b275a718794cf2dfee884d7173
jsebastianherrera <juansehgu@gmail.com>
2021-10-10 02:29:40 -0500

    query added

--- conexion.session.sql
+++ conexion.session.sql
@@ -3,12 +3,16 @@ SELECT k.id as carritoID,
     p.id as id_producto,
     p.nombre as producto,
     c.cantidad as cantidad,
-    p.precio as precio
+    p.precio as precio,
 from Producto p,
     Compra c,
     Carrito k
 where p.id = c.Productoid
     and c.Carritoid = k.id;
-------------------------
-Select *
-from Carrito_Productos;
+
+
+select p.nombre as producto,SUM(c.cantidad)*c.precio as total_producto,SUM(c.cantidad) cantidad
+from Carrito_Productos c, Producto p
+where c.id_producto =p.id
+group by c.carritoID,c.id_producto;
+
