74ef94d87024aad5b274fccf8fa46467937d3500
jsebastianherrera <juansehgu@gmail.com>
2021-10-16 17:22:48 -0500

    sql removed

--- jave_store/controller/apiDB.dart
+++ jave_store/controller/apiDB.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:jave_store/Entidades/Categoria.dart';
+
 List<ProductoxCarrito> ProductoxCarritoFromJson(String str) =>
     List<ProductoxCarrito>.from(
         json.decode(str).map((x) => ProductoxCarrito.fromJson(x)));
+
 class ProductoxCarrito {
   String nombre;
   String cantidad;
@@ -31,15 +33,17 @@ class ProductoxCarrito {
           nombre: json["producto"],
           precio: json["precio"]);
 }
+
 void main() {
   var url = "http://localhost/jave/queryDB.php";
   Future getData() async {
-     final response = await http.post(Uri.parse(url), body: {
+    final response = await http.post(Uri.parse(url), body: {
       "query":
           "select producto,sum(cantidad) as cantidad,precio from Carrito_Productos group by id_producto;"
     });
-    var rt= await CategoriaFromJson(response.body);
+    var rt = await CategoriaFromJson(response.body);
     print(rt);
   }
+
   getData();
 }
